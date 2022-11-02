e111217adae27e0ad8f589f9225903841d355e29
zach <zachshipko@gmail.com>
2022-11-01 20:06:53 -0700

    cleanup: remove Host/Variables

--- example/main.go
+++ example/main.go
@@ -8,8 +8,7 @@ import (
 
 //export count_vowels
 func count_vowels() int32 {
-	host := pdk.NewHost()
-	input := host.Input()
+	input := pdk.Input()
 
 	count := 0
 	for _, a := range input {
@@ -21,22 +20,21 @@ func count_vowels() int32 {
 	}
 
 	// test some extra pdk functionality
-	vars := host.Vars()
-	if vars.Get("a") == nil {
-		vars.Set("a", []byte("this is var a"))
+	if pdk.GetVar("a") == nil {
+		pdk.SetVar("a", []byte("this is var a"))
 	}
-	varA := vars.Get("a")
-	thing, ok := host.Config("thing")
+	varA := pdk.GetVar("a")
+	thing, ok := pdk.GetConfig("thing")
 
 	if !ok {
 		thing = "<unset by host>"
 	}
 
 	output := `{"count": ` + strconv.Itoa(count) + `, "config": "` + thing + `", "a": "` + string(varA) + `"}`
-	mem := host.AllocateString(output)
+	mem := pdk.AllocateString(output)
 
 	// zero-copy output to host
-	host.OutputMemory(mem)
+	pdk.OutputMemory(mem)
 
 	return 0
 }
