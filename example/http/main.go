75be75887865028b759985bf2cebe1a88f10b4bf
syke99 <quinnmillican@gmail.com>
2023-10-08 17:49:22 -0600

    fix example

--- example/http/main.go
+++ example/http/main.go
@@ -7,7 +7,7 @@ import (
 //export http_get
 func httpGet() int32 {
 	// create an HTTP Request (withuot relying on WASI), set headers as needed
-	req := pdk.NewHTTPRequest("GET", "https://jsonplaceholder.typicode.com/todos/1")
+	req := pdk.NewHTTPRequest(pdk.MethodGet, "https://jsonplaceholder.typicode.com/todos/1")
 	req.SetHeader("some-name", "some-value")
 	req.SetHeader("another", "again")
 	// send the request, get response back (can check status on response via res.Status())
