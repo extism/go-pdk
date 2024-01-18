f82506a1d0b5e4d5d4495a649480e8f2cf1c9296
Muhammad Azeez <muhammad@dylibso.com>
2024-01-18 20:50:07 +0300

    feat: create separate package to make reactor modules easier

---
+++ pdk_reactor/extism_pdk_reactor.go
@@ -0,0 +1,9 @@
+package pdk_reactor
+
+//export __wasm_call_ctors
+func __wasm_call_ctors()
+
+//export _initialize
+func _initialize() {
+	__wasm_call_ctors()
+}
---
+++ pdk_reactor/go.mod
@@ -0,0 +1,3 @@
+module github.com/extism/go-pdk/pdk_reactor
+
+go 1.21.1
