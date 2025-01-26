a1b0f27121fbb502390408fa542596d4f4e7b2cc
George Jenkins <gjenkins8@bloomberg.net>
2025-01-26 11:16:44 -0800

    feat: Create HTTPTransport
    Signed-off-by: George Jenkins <gjenkins8@bloomberg.net>

--- extism_pdk.go
+++ extism_pdk.go
@@ -1,8 +1,13 @@
 package pdk
 
 import (
+	"bytes"
 	"encoding/binary"
 	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
 )
 
 // Memory represents memory allocated by (and shared with) the host.
@@ -450,6 +455,103 @@ func (r *HTTPRequest) Send() HTTPResponse {
 		status,
 		headers,
 	}
+}
+
+// HTTPTransport implement go's http.RoundTripper interface, enabling usage of standard go
+// http.Client within a plugin
+type HTTPTransport struct {
+}
+
+func (t *HTTPTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+
+	convertRequestHeaders := func() map[string]string {
+		if len(req.Header) == 0 {
+			return nil
+		}
+
+		result := map[string]string{}
+
+		for name, values := range req.Header {
+			result[name] = strings.Join(values, ",")
+
+		}
+
+		return result
+	}
+
+	meta := HTTPRequestMeta{
+		URL:     req.URL.String(),
+		Headers: convertRequestHeaders(),
+		Method:  req.Method,
+	}
+
+	metaData, err := json.Marshal(meta)
+	if err != nil {
+		return nil, fmt.Errorf("failed to encode request headers: %q", err)
+	}
+
+	metaMemory := AllocateBytes(metaData)
+	defer metaMemory.Free()
+
+	var bodyMemoryOffset extismPointer
+	if req.Body != nil {
+		bodyData, err := io.ReadAll(req.Body)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read body bytes: %q", err)
+		}
+
+		bodyMemory := AllocateBytes(bodyData)
+		defer bodyMemory.Free()
+
+		bodyMemoryOffset = bodyMemory.offset
+	}
+
+	respPointer := extismHTTPRequest(metaMemory.offset, bodyMemoryOffset)
+	respLength := extismLengthUnsafe(respPointer)
+	respStatus := extismHTTPStatusCode()
+
+	headersPointer := extismHTTPHeaders()
+	respHeaders := map[string]string{}
+
+	if headersPointer != 0 {
+		headersLength := extismLengthUnsafe(headersPointer)
+		headersMemory := Memory{offset: headersPointer, length: headersLength}
+		defer headersMemory.Free()
+		json.Unmarshal(headersMemory.ReadBytes(), &respHeaders)
+	}
+
+	convertResponseHeaders := func() http.Header {
+		result := http.Header{}
+		for key, value := range respHeaders {
+			result.Add(key, value)
+		}
+
+		return result
+	}
+
+	resp := &http.Response{
+		Status:        http.StatusText(int(respStatus)),
+		StatusCode:    int(respStatus),
+		Proto:         "HTTP/1.1",
+		ProtoMajor:    1,
+		ProtoMinor:    1,
+		Header:        convertResponseHeaders(),
+		Body:          nil,
+		ContentLength: -1,
+		Request:       req,
+	}
+
+	hasBody := req.Method != "HEAD" && respLength > 0
+	if hasBody {
+		respMemory := Memory{respPointer, respLength}
+		respBuf := make([]byte, respMemory.length)
+		respMemory.Load(respBuf)
+
+		resp.Body = io.NopCloser(bytes.NewReader(respBuf))
+		resp.ContentLength = int64(respLength)
+	}
+
+	return resp, nil
 }
 
 // ReadBytes returns the host memory block as a slice of bytes.
